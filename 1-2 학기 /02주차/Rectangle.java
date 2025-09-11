c60471da90ccd685e9701bda35288be803d7ba62
Nirr-00 <skstkfrhqhwk@naver.com>
2025-09-11 16:14:26 +0900

    Add files via upload

---
+++ 1-2 학기 /02주차/Rectangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("높이 입력");
+        int height = scn.nextInt();
+
+        System.out.print("너비 입력");
+        int width = scn.nextInt();
+
+        int area = height * width;
+
+        System.out.println("높이 = " +height+ ", 너비 = " +width+ ", 인 사각형의 면적 = " +area);
+        scn.close();
+    }
+}
